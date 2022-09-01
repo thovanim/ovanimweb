8b1cf96caa3170f6c5809f6658b51dee93ec0e3f
dovanim <devops@ovanim.com>
2022-09-01 19:10:14 +0200

    Initial commit

---
+++ build/precache-manifest.dd4b88fba787f45e2b19e656e40657aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ed1db53dba1824cf6c0",
+    "url": "/ovanimweb/static/css/main.dcd3f8ef.chunk.css"
+  },
+  {
+    "revision": "9ed1db53dba1824cf6c0",
+    "url": "/ovanimweb/static/js/main.f3bb48a0.chunk.js"
+  },
+  {
+    "revision": "01d7544323ffef84c1b7",
+    "url": "/ovanimweb/static/js/runtime~main.5125996a.js"
+  },
+  {
+    "revision": "25614608c2b8864a3d36",
+    "url": "/ovanimweb/static/css/2.68550803.chunk.css"
+  },
+  {
+    "revision": "25614608c2b8864a3d36",
+    "url": "/ovanimweb/static/js/2.0221a4ab.chunk.js"
+  },
+  {
+    "revision": "50f4a02157d83cc5f2d4",
+    "url": "/ovanimweb/static/js/3.09a0fe09.chunk.js"
+  },
+  {
+    "revision": "f60a1dd87d11c2d021d5cc6bf9b620e6",
+    "url": "/ovanimweb/index.html"
+  }
+];
